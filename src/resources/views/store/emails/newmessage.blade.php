d803865125e54bcde111cedce1e013c64efd12d9
a4anthony <55565463+a4anthony@users.noreply.github.com>
2020-08-14 08:56:57 +0100

    initial commit

--- src/resources/views/store/emails/newmessage.blade.php
+++
@@ -1,7 +0,0 @@
-@php
-$body = str_replace(' ','%20',$template);
-@endphp
-
-<p>Name: {{$name}}</p>
-<p>Message: {{$message_content}}</p>
-<a href="mailto:{{$email}}?subject=Re: {{$subject}}&body={{$body}}">Click here to reply</a>
